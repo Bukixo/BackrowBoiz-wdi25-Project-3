c9643fccd067623de88dc6f2e5a06a8510f34763
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-03-24 15:40:28 +0000

    added error handler auth and main ctrl

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -1,8 +1,30 @@
 angular
   .module('rentApp')
-  .controller( 'MainCtrl', MainCtrl)
+  .controller( 'MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['$state'];
-function MainCtrl($state) {
+MainCtrl.$inject = ['$rootScope', '$state', '$auth'];
+function MainCtrl($rootScope, $state, $auth) {
   const vm = this;
+
+  vm.isAuthenticated = $auth.isAuthenticated;
+
+  vm.logout = logout;
+
+
+  function logout() {
+    $auth.logout(); //remove the token
+    $state.go('login');
+  }
+
+  $rootScope.$on('error', (e, err) => {
+    vm.stateHasChanged = false;
+    vm.message = err.data.message;
+    $state.go('login');
+  });
+
+  $rootScope.$on('$stateChangeSuccess', () => {
+    if(vm.stateHasChanged) vm.message = null;
+    if(!vm.stateHasChanged) vm.stateHasChanged = true;
+  });
+
 }
