ffb1a97f73ecdb6234aaee4dc932578479970ca9
Sam Domesjö <samdomesjo@gmail.com>
2017-03-24 13:51:56 +0000

    Login page

--- src/js/controllers/RegisterCtrl.js
+++ src/js/controllers/RegisterCtrl.js
@@ -5,4 +5,10 @@ angular
 RegisterCtrl.$inject = ['$state'];
 function RegisterCtrl($state){
   const vm = this;
+
+  vm.credentials; // Is going the be the ng-model form our formdata
+  function createUser(){
+    $state.go('login');
+  }
+  vm.createUser = createUser();
 }
