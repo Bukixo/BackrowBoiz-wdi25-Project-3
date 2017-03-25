0bf514852e3f4022ceceaa0d65c7bb341dc6b735
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-03-25 12:27:38 +0000

    upadating pages

--- src/js/controllers/IndexCtrl.js
+++ src/js/controllers/IndexCtrl.js
@@ -6,7 +6,9 @@ angular
 IndexCtrl.$inject = ['Item','User'];
 function IndexCtrl(Item, User) {
   const vm = this;
-  vm.items = Item.query();
-  vm.profiles = User.query(); // Remove this later it's just to see if the HTTP works
+
+  vm.all = Item.query();
+  // vm.items = Item.query();
+  // vm.profiles = User.query(); // Remove this later it's just to see if the HTTP works
 }
 // Item is injected from our Factory and makeing a GET request from the API /api/item to find all the items
