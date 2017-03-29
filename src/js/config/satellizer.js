915f53ae2b98d10454ab68b5b819c6bb6d068b08
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-03-29 12:44:36 +0100

    added a comment

--- src/js/config/satellizer.js
+++ src/js/config/satellizer.js
@@ -7,8 +7,6 @@ function Auth($authProvider) {
   $authProvider.signupUrl = '/api/register';
   $authProvider.loginUrl = '/api/login';
 
-
-
   $authProvider.github({
     clientId: '6fa44555d50ccd78c8df',
     url: '/api/oauth/github'
