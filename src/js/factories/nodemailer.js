6b1187e8e38fb82956bf9cfc29c8aec506c45d98
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-03-29 09:28:00 +0100

    starting nodemailer

---
+++ src/js/factories/nodemailer.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+// var nodemailer = require("nodemailer");
+
+const smtpTransport = nodemailer.createTransport('SMTP',{
+  service: 'Hotmail',  // sets automatically host, port and connection security settings
+  auth: {
+    user: 'hannahwynnjones@hotmail.co.uk',
+    pass: 'Skype386476'
+  }
+});
+
+console.log('SMTP Configured');
+
+smtpTransport.sendMail({  //email options
+  from: 'Hannah Nodemailer <hannahwynnjones@hotmail.co.uk>', // sender address.  Must be the same as authenticated user if using Gmail.
+  to: 'Receiver Name <hannahwynnjones2@gmail.com>', // receiver
+  subject: 'Emailing with nodemailer', // subject
+  text: 'Email Example with nodemailer' // body
+}, function(error, response){  //callback
+  if(error){
+    console.log(error);
+  }else{
+    console.log('Message sent: ' + response.message);
+  }
+
+  smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
+});
